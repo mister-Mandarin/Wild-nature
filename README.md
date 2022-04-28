3ab3c2c4dacc96fd08596156ba7d7fef8823f744
Andrey <andr.poluehktov@yandex.ru>
2022-04-28 20:41:44 +0300

    Complete 99,5%!!!

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Wild-nature
 
 # Начал проект после завершения всех тренажеров HTML academy 2022.03.30
+#
+# Завершил 2022.04.28. По ощущениям заняло около 30 часов.
+#
+#
 # Макет в фигме https://www.figma.com/file/Li5x8lbM6dGLgoy3VLK9BS/%5BPublished%5D%5BRU%5D-%C2%ABWild-nature-of-Russia%C2%BB-(Copy)
--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $('.trust-partners').slick({
     dots: false, //навигационные кнопки снизу
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: '<button class="button-slider" type="button"><img src="/img/bx_bx.png" alt=""></button>',
-    nextArrow: '<button class="button-slider btn-next" type="button"><img src="/img/arrow.svg" alt=""></button>',
+    prevArrow: '<button class="button-slider" type="button"><img src="/img/arrow.svg" alt="prew"></button>',
+    nextArrow: '<button class="button-slider btn-next" type="button"><img src="/img/arrow.svg" alt="next"></button>',
     responsive: [{
             breakpoint: 769,
             settings: {
